6613714c55db658b662262de5dc1e87406673c5a
Wubzy <peytonthegreat0@gmail.com>
2020-07-10 03:48:28 -0400

    Name Change

--- FunApp/Form1.Designer.cs
+++ FunApp/Form1.Designer.cs
@@ -34,9 +34,9 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(1902, 1033);
 			this.Name = "Form1";
-			this.Text = ":>";
+			this.Text = "Arjebie";
 			this.ResumeLayout(false);
 
 		}
